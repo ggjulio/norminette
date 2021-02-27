1998223b0559974ba6be170788768813ce1d296d
ggjulio <juligonz@student.42.fr>
2021-02-27 02:46:59 +0100

    add dockerhub image

---
+++ dockerhub/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+# set -ex
+
+## if command start with an option, directory or a file, prepend `norminette`
+# if [ -f "$1" -o -d "$1" -o "$(expr substr "$1" 1 1)" = '-' ]; then
+
+# if first arg not a command, and is either an option, file/dir or a mispelled word.
+# thus prepend `norminette` command
+if [ ! $(command -v "$1" &> /dev/null) ]; then
+	set -- norminette "$@"
+fi
+
+exec "$@"
