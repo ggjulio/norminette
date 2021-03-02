647ee4c428faf5a3918480e9ec689f08377b290c
ggjulio <juligonz@student.42.fr>
2021-03-02 10:48:50 +0100

    docker hub, update dockerfile to dl norminette from upstream

--- dockerhub/docker-entrypoint.sh
+++ dockerhub/docker-entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 ## if command start with an option, directory or a file, prepend `norminette`
 # if [ -f "$1" -o -d "$1" -o "$(expr substr "$1" 1 1)" = '-' ]; then
 
-# if first arg not a command, and is either an option, file/dir or a mispelled word.
+# if first arg is not a command, it's either an option, file/dir or a mispelled word.
 # thus prepend `norminette` command
 if [ ! $(command -v "$1" &> /dev/null) ]; then
 	set -- norminette "$@"
