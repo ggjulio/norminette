e651adf8c13daf86fb4e696d5347d9a00b0aec92
ggjulio <juligonz@student.42.fr>
2021-03-06 14:07:46 +0100

    .

--- norminette/run_test.sh
+++ norminette/run_test.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# echo "Running lexer unit test:"
+echo "Running lexer unit test:"
 python3 -m unittest discover tests/lexer/unit-tests/ "*.py"
-# echo "Running lexer test on files:"
+echo "Running lexer test on files:"
 python3 -m  tests.lexer.files.file_token_test
 python3 -m tests.lexer.errors.tester
 python3 -m tests.rules.rule_tester
