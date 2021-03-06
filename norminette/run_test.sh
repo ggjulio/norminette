f8109d583acbb043ac43524292bba10c555013ed
ggjulio <juligonz@student.42.fr>
2021-03-06 14:05:38 +0100

    add python3

--- norminette/run_test.sh
+++ norminette/run_test.sh
@@ -1,6 +1,10 @@
-echo "Running lexer unit test:"
-python -m unittest discover tests/lexer/unit-tests/ "*.py"
-echo "Running lexer test on files:"
-python -m  tests.lexer.files.file_token_test
-python -m tests.lexer.errors.tester
-python -m tests.rules.rule_tester
+#!/bin/env sh
+
+set -e
+
+# echo "Running lexer unit test:"
+python3 -m unittest discover tests/lexer/unit-tests/ "*.py"
+# echo "Running lexer test on files:"
+python3 -m  tests.lexer.files.file_token_test
+python3 -m tests.lexer.errors.tester
+python3 -m tests.rules.rule_tester
